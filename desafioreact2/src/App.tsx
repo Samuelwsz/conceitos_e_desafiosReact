3fc62820313ed372c8f49427781997b4a848f500
samuelwsz <samuelxwsz@gmail.com>
2023-09-02 16:35:15 -0300

    att

--- desafioreact2/src/App.tsx
+++ desafioreact2/src/App.tsx
@@ -49,41 +49,44 @@ function App() {
   }
 
   return (
-    <div className="wrapper">
-      <div className="login-form">
-        <h1>Login Form 🐞</h1>
-        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        {error && <div className="errorMessage">{error.message}</div>}
-        <div className="row">
-          <label htmlFor={"email"}>Email</label>
-          <input
-            id={"email"}
-            type={"email"}
-            autoComplete="off"
-            value={email}
-            onChange={handleEmail}
-          />
-        </div>
-        <div className="row">
-          <label htmlFor={"password"}>Password</label>
-          <input
-            id={"password"}
-            type={"password"}
-            value={password}
-            onChange={handlePassword}
-          />
-        </div>
+    <>
+      <div className="wrapper">
+        <div className="login-form">
+          <h1>Login Form 🐞</h1>
+          {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
+          {error && <div className="errorMessage">{error.message}</div>}
+          <div className="row">
+            <label htmlFor={"email"}>Email</label>
+            <input
+              id={"email"}
+              type={"email"}
+              autoComplete="off"
+              value={email}
+              onChange={handleEmail}
+            />
+          </div>
+          <div className="row">
+            <label htmlFor={"password"}>Password</label>
+            <input
+              id={"password"}
+              type={"password"}
+              value={password}
+              onChange={handlePassword}
+            />
+          </div>
 
-        <div className="button">
-          <button
-            onClick={handleSubmit}
-            disabled={email === "" || password.length < 6 || isRequesting}
-          >
-            Login
-          </button>
+          <div className="button">
+            <button
+              onClick={handleSubmit}
+              disabled={email === "" || password.length < 6 || isRequesting}
+            >
+              Login
+            </button>
+            <button>aaa</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
