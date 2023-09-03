4df9344fa9874a31f2bef4a22f9a8b68a0ba6852
samuelwsz <samuelxwsz@gmail.com>
2023-09-03 14:02:16 -0300

    att

---
+++ desafioreact3/src/App.tsx
@@ -0,0 +1,45 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h3>desafio fernandev</h3>
+      <h1>progresso do formulário</h1>
+
+      <main>
+        {/* crie a barra de progresso aqui */}
+        <div className="form-group">
+          <label htmlFor="">Nome Completo</label>
+          <input />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">E-mail</label>
+          <input />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Estado Civil</label>
+          <select>
+            <option value="">- selecione...</option>
+            <option value="solteiro">Solteiro</option>
+            <option value="casado">Casado</option>
+            <option value="divorciado">Divorciado</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Gênero</label>
+          <div className="radios-container">
+            <span>
+              <input type="radio" /> Masculino
+            </span>
+            <span>
+              <input type="radio" /> Feminino
+            </span>
+          </div>
+        </div>
+        <button>Enviar Formulário</button>
+      </main>
+    </div>
+  )
+}
+
+export default App
