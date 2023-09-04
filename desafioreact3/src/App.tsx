8cb56f12e5e31fbcd4568e3a40cc072f642bdbe1
samuelwsz <samuelxwsz@gmail.com>
2023-09-03 21:51:21 -0300

    att

--- desafioreact3/src/App.tsx
+++ desafioreact3/src/App.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [data, setData] = useState({
+    fullName: "",
+    email: "",
+    maritalStatus: "",
+    genre: "",
+  })
+
+  const handleChange = () => {}
+
   return (
     <div className="App">
       <h3>desafio fernandev</h3>
@@ -10,15 +20,23 @@ function App() {
         {/* crie a barra de progresso aqui */}
         <div className="form-group">
           <label htmlFor="">Nome Completo</label>
-          <input />
+          <input
+            name="fullName"
+            value={data.fullName}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="">E-mail</label>
-          <input />
+          <input name={"email"} value={data.email} onChange={handleChange} />
         </div>
         <div className="form-group">
           <label htmlFor="">Estado Civil</label>
-          <select>
+          <select
+            name="maritalStatus"
+            value={data.maritalStatus}
+            onChange={handleChange}
+          >
             <option value="">- selecione...</option>
             <option value="solteiro">Solteiro</option>
             <option value="casado">Casado</option>
@@ -29,10 +47,24 @@ function App() {
           <label htmlFor="">Gênero</label>
           <div className="radios-container">
             <span>
-              <input type="radio" /> Masculino
+              <input
+                type="radio"
+                name="genre"
+                value="masculino"
+                onChange={handleChange}
+                checked={data.genre === "masculino"}
+              />
+              Masculino
             </span>
             <span>
-              <input type="radio" /> Feminino
+              <input
+                type="radio"
+                name="genre"
+                value="feminino"
+                onChange={handleChange}
+                checked={data.genre === "feminino"}
+              />
+              Feminino
             </span>
           </div>
         </div>
