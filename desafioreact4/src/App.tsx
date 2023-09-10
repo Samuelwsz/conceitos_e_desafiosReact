3eca34b05d57c4a40a386886b4fec6f2eaf8d437
samuelwsz <samuelxwsz@gmail.com>
2023-09-10 15:29:52 -0300

    att

--- desafioreact4/src/App.tsx
+++ desafioreact4/src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
 
 const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
+  { indexes: [0, 1, 2], orientation: "horizontal" },
+  { indexes: [3, 4, 5], orientation: "horizontal" },
+  { indexes: [6, 7, 8], orientation: "horizontal" },
 
-  [0, 3, 6],
-  [1, 4, 7],
-  [3, 5, 8],
+  { indexes: [0, 3, 6], orientation: "vertical" },
+  { indexes: [1, 4, 7], orientation: "vertical" },
+  { indexes: [3, 5, 8], orientation: "vertical" },
 
-  [0, 4, 8],
-  [2, 4, 6],
+  { indexes: [0, 4, 8], orientation: "diagonal" },
+  { indexes: [2, 4, 6], orientation: "diagonal" },
 ]
 
 function App() {
@@ -19,11 +19,17 @@ function App() {
     0, 0, 0, 0, 0, 0, 0, 0, 0,
   ])
   const [turn, setTurn] = useState(1)
+  const [winningCombo, setWinningCombo] = useState<{
+    indexes: number[]
+    orientation: string
+  } | null>(null)
+  const [winnerGame, setWinner] = useState<string | null>()
 
   const handleClick = (clickedIndex: number) => {
-    console.log(clickedIndex)
-
-    if (gameData[clickedIndex]) {
+    if (gameData[clickedIndex] !== 0) {
+      return
+    }
+    if (winningCombo) {
       return
     }
 
@@ -33,7 +39,53 @@ function App() {
       return newGameData
     })
     setTurn((prev) => (prev === 1 ? 2 : 1))
+    console.log(turn)
   }
+
+  useEffect(() => {
+    checkWinner()
+    checkGameEnded()
+  }, [gameData])
+
+  useEffect(() => {
+    if (winningCombo) {
+      setWinner(turn !== 1 ? "player1" : "player2")
+      alert("Temos um vencedor")
+    }
+  }, [winningCombo])
+
+  const checkGameEnded = () => {
+    if (gameData.every((item) => item !== 0)) {
+      setWinner("Empate")
+      alert("Jogo acabou")
+    }
+  }
+
+  const checkWinner = () => {
+    let winner = null
+    for (let orientation of winningCombinations) {
+      const { indexes } = orientation
+      if (
+        gameData[indexes[0]] === 1 &&
+        gameData[indexes[1]] === 1 &&
+        gameData[indexes[2]] === 1
+      ) {
+        winner = "player1"
+      }
+      if (
+        gameData[indexes[0]] === 2 &&
+        gameData[indexes[1]] === 2 &&
+        gameData[indexes[2]] === 2
+      ) {
+        winner = "player2"
+      }
+      if (winner) {
+        setWinningCombo(orientation)
+        break
+      }
+    }
+  }
+
   return (
     <>
       <h3>desafio fernandev</h3>
@@ -45,6 +97,7 @@ function App() {
             onClick={() => {
               handleClick(index)
             }}
+            className="vertical"
           >
             <abbr title="">{index}</abbr>
             {value === 1 && "X"}
@@ -52,6 +105,7 @@ function App() {
           </span>
         ))}
       </div>
+      {winnerGame && <p>O vencedor é: {winnerGame}</p>}
     </>
   )
 }
